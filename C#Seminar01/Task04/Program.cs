1105cbb7a1cd37360d18b602a6fc0d3dfb74cc6a
statuspartner <vkuchevasov.sp@gmail.com>
2022-08-05 23:34:55 +0300

    Add Task04

---
+++ C#Seminar01/Task04/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите целое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+
+if(numberB > max)
+{
+    max = numberB;
+}
+if (numberC > max)
+{
+    max = numberC;
+}
+
+Console.Write("Максимальное число: ");
+Console.WriteLine(max);
