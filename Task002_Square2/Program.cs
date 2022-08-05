1bc22cb22f55d78685b132639fcc62b54c4eb80c
statuspartner <vkuchevasov.sp@gmail.com>
2022-08-05 22:43:04 +0300

    Add Task002_Square2

---
+++ Task002_Square2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите целое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if(numberA*numberA == numberB)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
