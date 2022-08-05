37471175d9aa6177d497302e6a99089d72102fc2
statuspartner <vkuchevasov.sp@gmail.com>
2022-08-05 23:48:56 +0300

    Add Task06

---
+++ C#Seminar01/Task6/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите целое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if(x % 2 == 0)
+{
+    Console.Write("Число ");
+    Console.Write(x);
+    Console.WriteLine(" - чётное.");
+}
+else
+{
+    Console.Write("Число ");
+    Console.Write(x);
+    Console.WriteLine(" - нечётное.");
+}
