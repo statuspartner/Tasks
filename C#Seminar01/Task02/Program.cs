6bae7461ecdeb1826f2f22b3b675b52714837ce6
statuspartner <vkuchevasov.sp@gmail.com>
2022-08-05 23:09:18 +0300

    Task02 is finished

--- C#Seminar01/Task02/Program.cs
+++ C#Seminar01/Task02/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("123Hello, World!");
+﻿Console.Write("Введите целое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int max;
+
+if(numberA > numberB)
+{
+    max = numberA;
+}
+else
+{
+    max = numberB;
+}
+
+Console.Write("Максимальное число: ");
+Console.WriteLine(max);
