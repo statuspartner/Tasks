c8b539c0a2d7c50adad892ee01e8da34e020a29a
statuspartner <vkuchevasov.sp@gmail.com>
2022-08-06 00:52:01 +0300

    Add Task08

---
+++ C#Seminar01/Task08/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите целое положительное число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+int even = 0;
+
+while(i <= x)
+{
+    if(i % 2 == 0)
+    {
+        Console.Write(even + i);
+        Console.WriteLine(" ");
+    }
+    i++;
+
+}
