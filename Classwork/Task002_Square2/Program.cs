fd391e57764041d3224b071c43ea4ed24b9c1b18
statuspartner <vkuchevasov.sp@gmail.com>
2022-08-07 16:02:39 +0300

    Add a little chande to code

--- Classwork/Task002_Square2/Program.cs
+++ Classwork/Task002_Square2/Program.cs
@@ -1,13 +1,13 @@
-﻿Console.Write("Введите целое число: ");
+﻿Console.Write("Введите целое число A: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите целое число: ");
+Console.Write("Введите целое число B: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
 
 if(numberB*numberB == numberA)
 {
-    Console.WriteLine("Да");
+    Console.WriteLine("Число A - квадрат числа B");
 }
 else
 {
-    Console.WriteLine("Нет");
+    Console.WriteLine("Число A - не квадрат числа B");
 }
