a0338ab5d761ce54cd7ac1110f3c481725567a6b
statuspartner <vkuchevasov.sp@gmail.com>
2022-08-10 20:16:41 +0300

    Add Task006

---
+++ Classwork/Task006/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if((numberA * numberA == numberB) || (numberB * numberB == numberA))
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
+
+/*
+Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if(numberA % 7 == 0 && numberA % 23 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
+
+Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if(numberA % numberB == 0)
+{
+    Console.WriteLine("Число B кратно числу A");
+}
+else
+{
+    Console.WriteLine("Число B не кратно числу A с остатком " + numberA % numberB);
+}
+*/
