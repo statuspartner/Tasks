6e203e9a7de1b0e563ab76ce7e1fee06501c20db
statuspartner <vkuchevasov.sp@gmail.com>
2022-08-10 20:26:20 +0300

    Add statement of problem to Task04

--- C#Seminar01/Task04/Program.cs
+++ C#Seminar01/Task04/Program.cs
@@ -1,4 +1,12 @@
-﻿Console.Write("Введите целое число: ");
+﻿/*
+Напишите программу, которая принимает на вход три числаи выдает
+максимальное из этих чисел.
+2,3,7 -> 7
+44,5,78 -> 78
+22,3,9 -> 22
+*/
+
+Console.Write("Введите целое число: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите целое число: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
