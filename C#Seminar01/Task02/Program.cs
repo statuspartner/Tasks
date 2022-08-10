e9536dd501a8a87896a337822425d1163181eee1
statuspartner <vkuchevasov.sp@gmail.com>
2022-08-10 20:47:48 +0300

    Teacher's version for Task02

--- C#Seminar01/Task02/Program.cs
+++ C#Seminar01/Task02/Program.cs
@@ -5,6 +5,8 @@ a = 5; b = 7 -> max = 7
 a = 2; b = 10 -> max = 10
 a = -9; b = -3 -> max = -3
 */
+
+/*
 Console.Write("Введите целое число: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите целое число: ");
@@ -22,3 +24,23 @@ else
 
 Console.Write("Максимальное число: ");
 Console.WriteLine(max);
+*/
+
+// Решение Преподавателя
+
+Console.WriteLine("Введите первое число: ");
+double a = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+double b = double.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+if(a > b)
+{
+    Console.WriteLine(a + " больше, чем " + b);
+}
+else
+{
+    Console.WriteLine(b + " больше, чем " + a);
+}
