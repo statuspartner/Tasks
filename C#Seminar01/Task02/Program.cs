2a8c625f98b1ec5776ca3bb4e38b4c5114438e3b
statuspartner <vkuchevasov.sp@gmail.com>
2022-08-10 20:13:27 +0300

    Add statement of problem

--- C#Seminar01/Task02/Program.cs
+++ C#Seminar01/Task02/Program.cs
@@ -1,4 +1,11 @@
-﻿Console.Write("Введите целое число: ");
+﻿/*
+Напишите программу, которая на вход принимает два числа
+и выдает, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2; b = 10 -> max = 10
+a = -9; b = -3 -> max = -3
+*/
+Console.Write("Введите целое число: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите целое число: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
