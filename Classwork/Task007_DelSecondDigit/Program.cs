f4ffea792e65e6bdac8437cf6266f04f3fce863c
statuspartner <vkuchevasov.sp@gmail.com>
2022-08-11 22:18:39 +0300

    Add Teacher's version

--- Classwork/Task007_DelSecondDigit/Program.cs
+++ Classwork/Task007_DelSecondDigit/Program.cs
@@ -13,3 +13,18 @@ int thirdDigit = randomDigit % 10;
 int digit = firstDigit + thirdDigit;
 
 Console.WriteLine("Магия удаления второй цифры: " + digit);
+
+*/
+
+// Решение Преподавателя
+
+int n = new Random().Next(100,1000);
+Console.WriteLine(n);
+
+int withoutsecond(int n)
+{
+    int first = n / 100;
+    int third = n % 10;
+    return first * 10 + third;
+}
+Console.WriteLine(withoutsecond(n));
