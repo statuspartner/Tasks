79253b64be4007b27412dc4a0b5211bb7fa4c47e
statuspartner <vkuchevasov.sp@gmail.com>
2022-08-11 22:48:56 +0300

    Add Task010_Square3

---
+++ Classwork/Task010_Square3/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Напишите программу, которая принимает на вход два числа и проверяет,
+является ли одно квадратом другого.
+5, 25 -> да
+-4, 16  -> да
+25, 5 -> да
+8, 9 -> нет
+*/
+
+Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if((numberA * numberA == numberB) || (numberB * numberB == numberA))
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
