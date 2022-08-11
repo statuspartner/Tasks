c64c78e0cd3127b28cd3e62db1a0499a22f3db4c
statuspartner <vkuchevasov.sp@gmail.com>
2022-08-11 22:14:23 +0300

    Add a little change for Task007_DelSecondDigit

--- Classwork/Task007_DelSecondDigit/Program.cs
+++ Classwork/Task007_DelSecondDigit/Program.cs
@@ -3,12 +3,13 @@
 и удаляет вторую цифру этого числа.
 */
 
+/*
 int randomDigit = new Random().Next(100, 999);
 
 Console.WriteLine($"Случайное число: {randomDigit}");
 
 int firstDigit = (randomDigit / 100) * 10;
-int secondDigit = randomDigit % 10;
-int digit = firstDigit + secondDigit;
+int thirdDigit = randomDigit % 10;
+int digit = firstDigit + thirdDigit;
 
 Console.WriteLine("Магия удаления второй цифры: " + digit);
