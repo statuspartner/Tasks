c4bee29baea89d7ceeb0d5ef93caf854ef6270f2
statuspartner <vkuchevasov.sp@gmail.com>
2022-08-11 22:04:23 +0300

    Add solution for Task007_DelSecondDigit

--- Classwork/Task007_DelSecondDigit/Program.cs
+++ Classwork/Task007_DelSecondDigit/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("434Hello, World!");
+﻿/*
+Напишите программу, которая выводит случайное трехзначное число
+и удаляет вторую цифру этого числа.
+*/
+
+int randomDigit = new Random().Next(100, 999);
+
+Console.WriteLine($"Случайное число: {randomDigit}");
+
+int firstDigit = (randomDigit / 100) * 10;
+int secondDigit = randomDigit % 10;
+int digit = firstDigit + secondDigit;
+
+Console.WriteLine("Магия удаления второй цифры: " + digit);
