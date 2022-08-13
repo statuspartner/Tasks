69ada948476fa14b3d3fe4f4a347fa64f429be89
statuspartner <vkuchevasov.sp@gmail.com>
2022-08-13 18:36:20 +0300

    Add some updates to code

--- C#Seminar03/Task19/Program.cs
+++ C#Seminar03/Task19/Program.cs
@@ -5,45 +5,27 @@
 12821 -> да
 23432 -> да
 */
+
 Console.Write("Введите кандидата на Палиндром: ");
 string palindrome = Console.ReadLine();
 int length = palindrome.Length;
-for(int i = 0; i < length/2; ++i)
+if(palindrome.Length == 5)
 {
-	if(palindrome[i] != palindrome[length-i-1])
-	{
-		Console.WriteLine(palindrome + " - это не палиндром.");
-		break;
-	}
-	else
-	{
-	    Console.WriteLine(palindrome + " - это палиндром.");
-		break;
-	}
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-Console.WriteLine("Введите целое пятизначное число: ");
-string word = Console.ReadLine();
-int len = word.length();
-
-for (int i = 0; i < len / 2; ++i)
-{
-    if (word[i] != word[len-i-1])
+    for(int i = 0; i < length/2; ++i)
     {
-        return false;
+    	if(palindrome[i] != palindrome[length-i-1])
+    	{
+    		Console.WriteLine(palindrome + " - это не палиндром.");
+    		break;
+    	}
+    	else
+    	{
+    	    Console.WriteLine(palindrome + " - это палиндром.");
+    		break;
+    	}
     }
 }
-*/
+else
+{
+    Console.WriteLine("Введите пятизначное число!");
+}
