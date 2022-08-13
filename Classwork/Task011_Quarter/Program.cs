acdaad913fbc4e6119ee18ba1da17751c6d0f2c7
statuspartner <vkuchevasov.sp@gmail.com>
2022-08-13 20:57:18 +0300

    Add Task011_Quarter

---
+++ Classwork/Task011_Quarter/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Напишите программу, которая принимает на вход координаты точки (X и Y)
+и выдает номер четверти плоскости, в которой находится эта точка.
+*/
+Console.Write("Введите X: ");
+int X = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Y: ");
+int Y = Convert.ToInt32(Console.ReadLine());
+
+if (X > 0 && Y > 0)
+{
+    Console.WriteLine("1-я четверть");
+}
+if (X < 0 && Y > 0)
+{
+    Console.WriteLine("2-я четверть");
+}
+if (X < 0 && Y < 0)
+{
+    Console.WriteLine("3-я четверть");
+}
+if (X > 0 && Y < 0)
+{
+    Console.WriteLine("4-я четверть");
+}
