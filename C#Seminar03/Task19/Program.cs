9651429c5404bb91a95d972234010f6e95ff00cc
Vladislav Kuchevasov <statuspartner@MacBook-Pro-Vladislav.local>
2022-08-17 22:04:47 +0300

    Add Teacher's solution

--- C#Seminar03/Task19/Program.cs
+++ C#Seminar03/Task19/Program.cs
@@ -6,7 +6,7 @@
 23432 -> да
 */
 
-Console.Write("Введите число из 5-ти знаков (кандидата на Палиндром): ");
+/* Console.Write("Введите число из 5-ти знаков (кандидата на Палиндром): ");
 string palindrome = Console.ReadLine();
 int length = palindrome.Length;
 if(palindrome.Length == 5)
@@ -28,4 +28,27 @@ if(palindrome.Length == 5)
 else
 {
     Console.WriteLine("Введите пятизначное число!");
+} */
+
+// Решение преподавателя
+
+int a = int.Parse(Console.ReadLine());
+
+if (a < 99999 & a >= 10000) // условие для проверки, является ли число пятизначным
+{
+	int xx = a / 1000;	// 1,2 цифры
+	int num1 = xx / 10; // 1 цифра
+	int num2 = xx % 10; // 2 цифра
+	int yy = a % 100;   // 4,5 цифры
+	int num4 = yy / 10; // 4 цифра
+	int num5 = yy % 10; // 5 цифра
+
+	if (num1 == num5 & num2 == num4)
+	{
+		Console.WriteLine($"Число {a} - палиграм.");
+	}
+	else
+	{
+		Console.WriteLine($"Число {a} - не палиграм.");
+	}
 }
