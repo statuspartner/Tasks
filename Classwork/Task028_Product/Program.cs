914ec79ef0b1d9764860e5b5c1675a2952616018
Vladislav Kuchevasov <statuspartner@MacBook-Pro-Vladislav.local>
2022-08-18 19:54:14 +0300

    Add Task 28

---
+++ Classwork/Task028_Product/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число N и выдает произведение чисел от 1 до N
+// 4 -> 24; 5 -> 120.
+/* Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+int product = 1;
+
+while (count <= N)
+{
+    product = product * count;
+    count++;
+}
+Console.WriteLine(product); */
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int mul = 1;
+for (int i = 1; i <= N; i++)
+{
+    mul *= i;
+}
+Console.WriteLine(mul);
