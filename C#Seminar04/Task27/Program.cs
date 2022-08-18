84bb91ce2ddfe5d20ee0f0633ca307e4b573951c
Vladislav Kuchevasov <statuspartner@MacBook-Pro-Vladislav.local>
2022-08-18 19:06:38 +0300

    Add Task 26

---
+++ C#Seminar04/Task27/Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("3333Hello, World!");
---
+++ Classwork/Task26_Number/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число и выдает количество цифр в числе.
+// 456 -> 3; 78 -> 2; 89126 -> 5.
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+while (num != 0)
+{
+    count++;
+    num = num / 10;
+}
+
+Console.WriteLine(count);
