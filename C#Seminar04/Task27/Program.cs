0d51de64aa741d72cefd677fe6298967be589a98
Vladislav Kuchevasov <statuspartner@MacBook-Pro-Vladislav.local>
2022-08-19 14:22:43 +0300

    Add Task27

--- C#Seminar04/Task27/Program.cs
+++ C#Seminar04/Task27/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("3333Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и выдает сумму цифр в числе.
+// 452 -> 11; 82 -> 10; 9012 -> 12.
+
+Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int sumDigit = 0;
+
+while (num != 0)
+{
+    sumDigit = sumDigit + (num % 10);
+    num /= 10;
+}
+Console.WriteLine(sumDigit);
