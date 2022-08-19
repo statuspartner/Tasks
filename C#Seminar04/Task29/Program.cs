2a3a1c322fd4a6bee2805d87e1caaf1beffab59b
Vladislav Kuchevasov <statuspartner@MacBook-Pro-Vladislav.local>
2022-08-19 14:47:00 +0300

    Add Task29

--- C#Seminar04/Task29/Program.cs
+++ C#Seminar04/Task29/Program.cs
@@ -1,14 +1,20 @@
-﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+﻿// Напишите программу, которая задает массив из 8 элементов и выводит их на экран. 
 // 1,2,5,7,19 -> [1,2,5,7,19]; 6,1,33 -> [6,1,33]
 
-int[] array = new int [8];
+Console.Write("Введите количество элементов массива: ");
+int arrDigits = int.Parse(Console.ReadLine());
+
+int[] array = new int [arrDigits];
 int len = array.Length;
 int i = 0;
 while (i < len)
 {
+    Console.Write($"Введите {i+1} элемент массива: ");
     array[i] = Convert.ToInt32(Console.ReadLine());
     i++;
 }
+Console.WriteLine();
+Console.Write("Готовый массив с элементами: ");
 for (int j = 0; j < len; j++)
 {
     Console.Write(array[j] + " ");
