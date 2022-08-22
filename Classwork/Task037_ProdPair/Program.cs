a6a4453f92a5666f5b9e27851f662d59ff2f5199
Vladislav Kuchevasov <statuspartner@MacBook-Pro-Vladislav.local>
2022-08-22 11:49:20 +0300

    Add Task037

---
+++ Classwork/Task037_ProdPair/Program.cs
@@ -0,0 +1,38 @@
+﻿// Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент,
+// второй и предпоследний и т.д. Результат запишите в новом массиве.
+// [1,2,3,4,5] -> 5 8 3; [6,7,3,6] ->  36 21
+
+int[] array = {1, 2, 3, 4, 5};
+int len = array.Length;
+int index = 0;
+
+// Вывод массива.
+Console.Write("Массив = ");
+while (index < len)
+{
+    Console.Write(array[index]);
+    Console.Write(" ");
+    index++;
+}
+Console.WriteLine();
+
+// Находим пары, перемножаем и выводим.
+index = 0;
+if (len % 2 == 0)
+{
+    while (index < len / 2)
+    {
+        Console.Write(array[index] * array[len - index - 1] + " ");
+        index++;
+    }
+}
+else
+{
+    while (index < len / 2)
+    {
+        Console.Write(array[index] * array[len - index - 1] + " ");
+        index++;
+    }
+    Console.Write(array[index]);
+}
+Console.WriteLine();
