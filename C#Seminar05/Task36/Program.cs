ead4a1caf33c0eff50e61e0524bfc5b49b1cc0f0
Vladislav Kuchevasov <statuspartner@MacBook-Pro-Vladislav.local>
2022-08-22 16:40:57 +0300

    Add Task036

---
+++ C#Seminar05/Task36/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечетных позициях.
+// [3, 7, 23, 12] -> 19; [-4, -6, 89, 6] -> 0
+
+int[] array = new int [6];
+int len = array.Length;
+int i = 0;
+int sumNegIndexes = 0;
+int count = 0;
+
+Console.Write("Массив: ");
+while (i < len)
+{
+    array[i] = new Random().Next(-100, 100);
+    Console.Write(array[i] + " ");
+    i++;
+}
+Console.Write(" \n");
+Console.Write("Элементы, удовлетворящие условию задачи: ");
+for (int j = 0; j < len; j++)
+{
+    if (j % 2 != 0)
+    {
+        sumNegIndexes = sumNegIndexes + array[j];
+        count++;
+        Console.Write(array[j] + " ");
+    }
+}
+Console.Write(" \n");
+Console.WriteLine("Сумма элементов с нечетными индексами массива: " + sumNegIndexes);
