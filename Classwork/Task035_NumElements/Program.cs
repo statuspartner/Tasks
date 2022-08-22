817848673579fe1c363d5e8a53fdb79f8f7d6c0b
Vladislav Kuchevasov <statuspartner@MacBook-Pro-Vladislav.local>
2022-08-22 10:30:12 +0300

    Add Task035

---
+++ Classwork/Task035_NumElements/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива
+// значения которых лежат в отрезке [10,99]
+// Пример для массива из 5-ти: [5,18,23,6,2] -> 1; [1,2,3,6,2] -> 0; [10,11,12,13,14] -> 5
+
+int[] array = new int[123];
+int length = array.Length;
+int i = 0;
+int count = 0;
+
+while(i < length)
+{
+    array[i] = new Random().Next(0, 200);
+    Console.Write(array[i] + " ");
+    i++;    
+}
+Console.WriteLine(" \n");
+
+for (int j = 0; j < length; j++)
+{
+    if(array[j] > 9 & array[j] < 100)
+    {
+        // Console.Write(array[j] + " ");
+        count++;
+        Console.Write(array[j] + " ");
+    }
+    
+    // else
+    // {
+    //     Console.WriteLine("Нет такого.");
+    //     break;
+    // }
+    
+}
+Console.WriteLine(" \n");
+Console.Write("Количество найденных элементов массива: " + count);
+Console.WriteLine(" \n");
