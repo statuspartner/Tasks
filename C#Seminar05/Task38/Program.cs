a83c8c57c3ef8cd4db05ef17b87b6a6939cd55a9
Vladislav Kuchevasov <statuspartner@MacBook-Pro-Vladislav.local>
2022-08-23 19:58:41 +0300

    Add Task038

---
+++ C#Seminar05/Task38/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементом массива.
+// [3, 7, 22, 2, 78] -> 76
+
+double[] array = new double [5];
+int len = array.Length;
+int i = 0;
+double min = 100;
+double max = 0;
+
+Console.Write("Массив: ");
+while (i < len)
+{
+    // array[i] = new Random().NextDouble() * 200 - 100;
+    array[i] = new Random().NextDouble() * 100;
+    Console.Write(Math.Round(array[i], 2) + "| ");
+    i++;
+}
+for (i = 0; i < len; i++)
+{
+    if (array[i] > max) max = array[i];
+    if (array[i] < min) min = array[i];
+}
+
+Console.Write(" \n");
+Console.Write("Max result: " + Math.Round(max,2));
+Console.Write(" \n");
+Console.Write("Min result: " + Math.Round(min,2));
+Console.Write(" \n");
+Console.WriteLine("Разница: " + Math.Round(max - min, 2));
