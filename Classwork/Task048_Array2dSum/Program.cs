ac397c88348dfd47fe80c0605097c8ff6baea803
Vladislav Kuchevasov <statuspartner@MacBook-Pro-Vladislav.local>
2022-08-27 19:02:27 +0300

    Add Task048 Array2dSum

---
+++ Classwork/Task048_Array2dSum/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 48: Задайте двумерный массив размером m x n,
+// каждый элемент массива находится по формуле Anm = m + n.
+// Выведите полученный массив на экран. 
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+Console.Write("Введите число m: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = int.Parse(Console.ReadLine());
+
+
+int[,] arr = new int[m, n];
+
+void FillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            arr [i, j] = i + j;
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            {Console.Write($"{arr[i, j]}\t");}
+        Console.WriteLine();
+    }
+}
+
+FillArray(arr);
+PrintArray(arr);
