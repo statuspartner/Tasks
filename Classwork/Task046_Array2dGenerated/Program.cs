2819a56ce929b0e177d6e93ef6129578d9dbd33c
Vladislav Kuchevasov <statuspartner@MacBook-Pro-Vladislav.local>
2022-08-27 18:41:26 +0300

    Add Task046 Array2d Generated

---
+++ Classwork/Task046_Array2dGenerated/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 46: Задайте двумерный массив размером m x n,
+// заполненный случайными целыми числами. 
+// m = 3, n = 4.
+// 1  4  8 19
+// 5  -2 33  -2
+// 77  3  8   1
+
+Console.Write("Введите число m: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = int.Parse(Console.ReadLine());
+
+
+int[,] arr = new int[m, n];
+
+void FillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            arr [i, j] = new Random().Next (-9,40);
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            {Console.Write($"{arr[i, j]}\t");}
+        Console.WriteLine();
+    }
+}
+
+FillArray(arr);
+PrintArray(arr);
