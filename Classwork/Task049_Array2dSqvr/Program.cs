a0e6387ee11017ce63f3777c9476eb59e093a78c
Vladislav Kuchevasov <statuspartner@MacBook-Pro-Vladislav.local>
2022-08-27 19:51:56 +0300

    Add Task049 Array2d Sqvr

---
+++ Classwork/Task049_Array2dSqvr/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 49: Задайте двумерный массив.
+// Найдите элементы, у которых оба индекса четные
+// и замените эти элементы на их квадраты.
+// 6 4 7 2      36 4 49 2
+// 5 9 2 3       5 9  2 3
+// 8 4 2 4      64 4  4 4
+
+Console.Write("Введите число m: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = int.Parse(Console.ReadLine());
+
+
+int[,] array = new int[m, n];
+
+void FillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            arr [i, j] = new Random().Next (1,10);
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            {Console.Write($"{arr[i, j]}\t");}
+        Console.WriteLine();
+    }
+}
+
+void SqrEven(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i += 2)
+    {
+        for (int j = 0; j < arr.GetLength(1); j += 2)
+        {
+            arr [i, j] = arr [i, j] * arr [i, j];
+        } 
+    }
+}
+
+FillArray(array);
+PrintArray(array);
+SqrEven(array);
+Console.WriteLine();
+PrintArray(array);
