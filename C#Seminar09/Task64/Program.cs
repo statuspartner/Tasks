d791519e13f253ec77e1f7d98803e90f6a470e6b
Vladislav Kuchevasov <statuspartner@MacBook-Pro-Vladislav.local>
2022-09-04 22:05:05 +0300

    Add Task064

---
+++ C#Seminar09/Task64/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1 (используйте рекурсию).
+// N = 5 -> 5, 4, 3, 2, 1
+
+Console.Clear();
+
+int n = InputNumbers("Задайте целое положительное число: ");
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+if(n < 0) Console.WriteLine("Введите положительное число!");
+
+void RecursionNumbers(int n)
+{
+    if (n > 0)
+    {
+        Console.Write("{0} ", n);
+        RecursionNumbers(n - 1);
+    }
+}
+
+int recNum = n;
+RecursionNumbers(recNum);
+
