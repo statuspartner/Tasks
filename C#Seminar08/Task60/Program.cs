1e640882d023d2c060e087bca67b5085742c568d
Vladislav Kuchevasov <statuspartner@MacBook-Pro-Vladislav.local>
2022-09-04 14:42:36 +0300

    Add Task 60

---
+++ C#Seminar08/Task60/Program.cs
@@ -0,0 +1,53 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+Console.Clear();
+int a = InputNumbers("Введите число строк массива: ");
+int b = InputNumbers("Введите число столбцов массива: ");
+int c = InputNumbers("Введите число уровней глубины массива: ");
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+void FillArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i,j,k] = new Random().Next(10, 99);
+            }
+        }
+    }
+}
+
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write(array[i,j,k] + "(" + i + "," + j + "," + k + ")" + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+int[,,] threeDArray = new int[a, b, c];
+FillArray(threeDArray);
+Console.WriteLine($"\nТрёхмерный массив:");
+PrintArray(threeDArray);
