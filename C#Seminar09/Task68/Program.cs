fe3df0da0b78773c9c7cb3907298b386ec941dd9
Vladislav Kuchevasov <statuspartner@MacBook-Pro-Vladislav.local>
2022-09-05 12:05:35 +0300

    Add a little more changes to Task068

--- C#Seminar09/Task68/Program.cs
+++ C#Seminar09/Task68/Program.cs
@@ -18,12 +18,9 @@ int InputNumbers(string input)
 
 int AckermannFunction(int n, int m)
 {
-if (n == 0)
-    return m + 1;
-else if ((n != 0) && (m == 0))
-    return AckermannFunction(n - 1, 1);
-else
-    return AckermannFunction(n - 1, AckermannFunction(n, m - 1));
+if (n == 0) return m + 1;
+else if ((n != 0) && (m == 0)) return AckermannFunction(n - 1, 1);
+return AckermannFunction(n - 1, AckermannFunction(n, m - 1));
 }
 
 Console.WriteLine(AckermannFunction(m, n));
