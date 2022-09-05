b0f536359b3c55495c266417d9c2325ac683d3ed
Vladislav Kuchevasov <statuspartner@MacBook-Pro-Vladislav.local>
2022-09-05 21:18:28 +0300

    Add some changes to Task60

--- C#Seminar08/Task60/Program.cs
+++ C#Seminar08/Task60/Program.cs
@@ -34,13 +34,13 @@ void FillArray(int[,,] array)
 
 void PrintArray(int[,,] array)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+    for (int i = 0; i < array.GetLength(2); i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
+        for (int j = 0; j < array.GetLength(0); j++)
         {
-            for (int k = 0; k < array.GetLength(2); k++)
+            for (int k = 0; k < array.GetLength(1); k++)
             {
-                Console.Write(array[i,j,k] + "(" + i + "," + j + "," + k + ")" + " ");
+                Console.Write(array[i,j,k] + "(" + i + ", " + j + ", " + k + ")" + " ");
             }
             Console.WriteLine();
         }
